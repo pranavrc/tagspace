fbda1d94a2e5c4f43c252e1b0f66aacf3b75a96c
Pranav Ravichandran <me@onloop.net>
2015-10-29 18:54:29 -0400

    Doc

--- tagspace.py
+++ tagspace.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+'''
+https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
+
+Script that prints the titles of every article from a start
+article on the way to Philosophy.
+'''
+
 from bs4 import BeautifulSoup
 import urllib2
 import wikipedia
@@ -7,19 +14,23 @@ import re
 import sys
 
 class ExtractLink:
+    ''' Class containing subroutines to parse html
+    and return the first valid link in a Wikipedia article.'''
     def __init__(self, article_html):
         self.soup = BeautifulSoup(article_html, "html.parser")
         self.soup = self.soup.find(id="mw-content-text")
 
     def clean_parantheses(self, content):
+        ''' Remove content inside parantheses. '''
         return re.sub(r'\([^)]*\)', '', content)
 
     def is_valid_link(self, href, paragraph):
-        if not href or "#" in href or "//" in href or ":" in href:
-            return False
-        if "/wiki/" not in href:
-            return False
-        if href not in paragraph:
+        ''' A link is valid only if:
+            1. It is not an external link or an anchor or a Help link.
+            2. It is not within parantheses.
+        '''
+        if not href or "#" in href or "//" in href or ":" in href \
+        or "/wiki/" not in href or href not in paragraph:
             return False
         
         prefix = paragraph.split(href, 1)[0]
@@ -30,14 +41,18 @@ class ExtractLink:
         return True
 
     def is_valid_tag(self, tag):
+        ''' The only valid tags are <p> and <ul> tags. '''
         return tag.name == 'p' or tag.name == 'ul'
 
     def get_paragraphs(self):
+        ''' Get all valid, top-level paragraphs from the article. '''
         self.paragraphs = self.soup.find_all(self.is_valid_tag, recursive=False)
 
         return self.paragraphs
 
     def iterate_links(self):
+        ''' Iterate through all links in each paragraph
+            and output the first valid link encountered. '''
         for each_paragraph in self.paragraphs:
             for each_link in each_paragraph.find_all("a"):
                 href = each_link.get("href")
@@ -47,6 +62,8 @@ class ExtractLink:
         return False
 
     def get_first_link(self):
+        ''' Wrapper function to run other subroutines and
+            output a dictionary of info about the next article. '''
         self.get_paragraphs()
         first_link = self.iterate_links()
 
@@ -55,22 +72,29 @@ class ExtractLink:
                 "text": first_link.get('text')}
 
 class IterateArticles:
+    ''' Class with subroutines to traverse the first links from
+    the start article to Philosophy. '''
     def __init__(self, start_page):
         self.start_page = start_page
         self.base_url = "http://en.wikipedia.org/w/index.php?title="
 
     def get_page(self, address):
+        ''' Fetch html of the page from address. '''
         req = urllib2.Request(address)
         data = urllib2.urlopen(req).read()
         return data
 
     def traverse(self):
+        ''' Traverse the first links from the start article,
+        recursively until Philosophy is reached. '''
         page_name = self.start_page
         page_url = self.base_url + page_name + "&printable=yes"
         results = []
 
         while True:
-            print page_name
+            sys.stdout.write(page_name + ' -> ')
+            sys.stdout.flush()
+
             results.append(page_name)
 
             article_html = self.get_page(page_url)
@@ -88,7 +112,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 0:
         try:
             results = IterateArticles(sys.argv[1])
-            print results.traverse()
+            results.traverse()
         except IndexError:
             print 'Format: python tagspace.py <page name>'
     else:
