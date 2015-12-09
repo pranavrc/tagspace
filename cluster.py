0ae4d208b9c9b1155d3fd813f0574289ee53dc36
Pranav Ravichandran <me@onloop.net>
2015-12-09 02:41:13 -0500

    most_common_ancestors() and segment_sessions()

--- cluster.py
+++ cluster.py
@@ -3,6 +3,7 @@
 import igraph as ig
 import tagspace as ts
 import itertools
+import operator
 
 class ClusterTopics:
     def __init__(self, topics):
@@ -18,14 +19,15 @@ class ClusterTopics:
     def assign_edge_weights(self):
         vertices = self.graph.vs["name"]
         vertex_pairs = list(itertools.combinations(vertices, 2))
-        
+
         for pair in vertex_pairs:
             self.graph[pair[0], pair[1]] = topic_sim(pair[0], pair[1])
-        
+
         return self.graph
 
     def cluster(self):
         return self.graph.community_fastgreedy()
+
 
 class TopicRelation:
     def __init__(self, articles):
@@ -40,3 +42,31 @@ class TopicRelation:
         for article in self.articles:
             article_paths[article] = ts.IterateArticles(article).traverse()
         return article_paths
+
+    def most_common_ancestor(self):
+        ancestors = {}
+        article_path_list = self.article_paths.values()
+        for path in article_path_list:
+            for topic_idx in range(path):
+                if path[topic_idx] in ancestors:
+                    ancestors[path[topic_idx]] += 1 / (1 + topic_idx)
+                else:
+                    ancestors[path[topic_idx]] = 1 / (1 + topic_idx)
+
+        return max(ancestors.iteritems(), key=operator.itemgetter(1))[0]
+
+
+def segment_sessions(queries, session_interval, accumulator=[]):
+    end_time = queries[0][1] + session_interval
+    session_list = []
+
+    for query_idx in range(queries):
+        if queries[query_idx][1] <= end_time:
+            session_list.append(queries[query_idx][0])
+        else:
+            return segment_sessions(queries[query_idx:],
+                                    session_interval,
+                                    accumulator.append(session_list))
+
+    return accumulator.append(session_list)
+
