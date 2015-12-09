f4db3f1a30cc390d1082d83f44f6f6d6d920dc53
Pranav Ravichandran <me@onloop.net>
2015-12-09 18:30:41 -0500

    Doccing up

--- cluster.py
+++ cluster.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+'''
+Modules to build graphs out of sessions of visited topics
+and cluster them based on ancestors in the Philosophy tree.
+'''
+
 import igraph as ig
 import tagspace as ts
 import itertools
@@ -7,6 +12,8 @@ import operator
 from math import log10
 
 class ClusterTopics:
+    ''' Class containing subroutines to build a full graph,
+    assign edge weights and apply Newman clustering. '''
     def __init__(self, topics):
         self.topics = topics
         self.graph = self.make_complete_graph()
@@ -14,12 +21,14 @@ class ClusterTopics:
         self.cluster = self.cluster()
 
     def make_complete_graph(self):
+        ''' Make a complete graph of topics with initial unit weights. '''
         graph = ig.Graph.Full(len(self.topics))
         graph.vs["name"] = self.topics
         graph.es["weight"] = 1.0
         return graph
 
     def assign_edge_weights(self):
+        ''' Assign edge weights between nodes based on common ancestors. '''
         vertices = self.graph.vs["name"]
         vertex_pairs = list(itertools.combinations(vertices, 2))
 
@@ -29,10 +38,13 @@ class ClusterTopics:
         return self.graph
 
     def cluster(self):
+        ''' Run Newman clustering on the graph. '''
         return self.graph.community_fastgreedy()
 
 
 class TopicRelation:
+    ''' Class containing subroutines to segment article sessions
+    and associate sessions with tags. '''
     def __init__(self, queries, session_interval):
         self.queries = segment_sessions(queries, session_interval)
 
@@ -44,14 +56,16 @@ class TopicRelation:
         for path in self.article_paths:
             self.topic_weights.update(self.most_common_ancestor(path))
 
-
     def build_paths(self, articles):
+        ''' Build paths to philosophy for a set of articles. '''
         article_paths = {}
         for article in articles:
             article_paths[article] = ts.IterateArticles(article).traverse()
         return article_paths
 
     def most_common_ancestor(self, article_paths):
+        ''' Find the most common ancestor of several article paths
+        based on the path to Philosophy. '''
         ancestors = {}
         article_path_list = article_paths.values()
 
@@ -67,6 +81,8 @@ class TopicRelation:
 
 
 def segment_sessions(queries, session_interval, accumulator=[]):
+    ''' Segment a list of queries into sessions, each
+    spanning a session interval. '''
     end_time = queries[0][1] + session_interval
     session_list = []
 
@@ -81,6 +97,7 @@ def segment_sessions(queries, session_interval, accumulator=[]):
     return accumulator + [session_list]
 
 def find_intersection(topic_x, topic_y):
+    ''' Find the intersection of two article paths to Philosophy. '''
     results_x = ts.IterateArticles(topic_x).traverse()
     results_y = ts.IterateArticles(topic_y).traverse()
     return set(results_x).intersection(results_y)
