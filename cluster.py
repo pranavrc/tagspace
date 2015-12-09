064033ce69c3a74e50a2fdb7bd85a4a3df29fdb3
Pranav Ravichandran <me@onloop.net>
2015-12-09 17:12:15 -0500

    Move find_intersection() out of class

--- cluster.py
+++ cluster.py
@@ -9,20 +9,21 @@ from math import log10
 class ClusterTopics:
     def __init__(self, topics):
         self.topics = topics
-        self.graph = None
+        self.graph = self.make_complete_graph()
+        self.assign_edge_weights()
 
     def make_complete_graph(self):
-        self.graph = ig.Graph.Full(len(self.topics))
-        self.graph.vs["name"] = self.topics
-        self.graph.es["weight"] = 1.0
-        return self.graph
+        graph = ig.Graph.Full(len(self.topics))
+        graph.vs["name"] = self.topics
+        graph.es["weight"] = 1.0
+        return graph
 
     def assign_edge_weights(self):
         vertices = self.graph.vs["name"]
         vertex_pairs = list(itertools.combinations(vertices, 2))
 
         for pair in vertex_pairs:
-            self.graph[pair[0], pair[1]] = topic_sim(pair[0], pair[1])
+            self.graph[pair[0], pair[1]] = len(find_intersection(pair[0], pair[1]))
 
         return self.graph
 
@@ -31,23 +32,27 @@ class ClusterTopics:
 
 
 class TopicRelation:
-    def __init__(self, articles):
-        self.articles = articles
-        self.article_paths = self.build_paths()
+    def __init__(self, queries, session_interval):
+        self.queries = segment_sessions(queries, session_interval)
+
+        self.article_paths = []
+        for session in self.queries:
+            self.article_paths.append(self.build_paths(session))
+
         self.topic_weights = {}
+        for path in self.article_paths:
+            self.topic_weights.update(self.most_common_ancestor(path))
 
-    def find_intersection(self, results_x, results_y):
-        return set(results_x).intersection(results_y)
 
-    def build_paths(self):
+    def build_paths(self, articles):
         article_paths = {}
-        for article in self.articles:
+        for article in articles:
             article_paths[article] = ts.IterateArticles(article).traverse()
         return article_paths
 
-    def most_common_ancestor(self):
+    def most_common_ancestor(self, article_paths):
         ancestors = {}
-        article_path_list = self.article_paths.values()
+        article_path_list = article_paths.values()
 
         for path in article_path_list:
             for topic_idx in range(1, len(path)):
@@ -57,9 +62,7 @@ class TopicRelation:
                     ancestors[path[topic_idx]] = 3.0 / topic_idx
 
         max_topic = max(ancestors.iteritems(), key=operator.itemgetter(1))[0]
-        self.topic_weights[max_topic] = ancestors[max_topic]
-
-        return self.topic_weights
+        return {max_topic: ancestors[max_topic]}
 
 
 def segment_sessions(queries, session_interval, accumulator=[]):
@@ -75,4 +78,9 @@ def segment_sessions(queries, session_interval, accumulator=[]):
                                     accumulator=accumulator + [session_list])
 
     return accumulator + [session_list]
+
+def find_intersection(topic_x, topic_y):
+    results_x = ts.IterateArticles(topic_x).traverse()
+    results_y = ts.IterateArticles(topic_y).traverse()
+    return set(results_x).intersection(results_y)
 
